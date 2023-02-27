9569d5372bf52f914da493a8bae69f0294f31351
Valeriya Dashdamirova <valerya.dt@yandex.ru>
2023-02-27 03:22:32 +0300

    update footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,14 +5,15 @@ import EiScVk from "./icones/EiScVk";
 import EiScTelegram from "./icones/EiScTelegram";
 import EiScYoutube from "./icones/EiScYoutube";
 import Logotype from "../logotype/logotype";
+import CarbonSendAlt from "./icones/CarbonSendAlt";
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__body">
                 <div className="footer__block">
-                    <div className="pb-4 flex items-start w-full"><Logotype /></div>
-                    <div className="text-sm w-40 h-auto text-justify font-medium text-white/70">Lorem ipsum dolor sit amet, consectetur adipiscing
+                    <div className="footer__logo"><Logotype /></div>
+                    <div className="footer__about">Lorem ipsum dolor sit amet, consectetur adipiscing
                         elit. Vivamus ut interdum magna, ac
                         scelerisque sapien.
                     </div>
@@ -20,27 +21,34 @@ function Footer() {
                 <div className="footer__block">
                     <div className="footer__block-name">Contact Info</div>
                     <div className="footer__block-links">
-                        <div className="footer__block-link text-start text-sm">Location: Moscow, Russia</div>
+                        <div className="footer__block-link footer__link-location">Location: Moscow, Russia</div>
                         <div className="footer__block-link">Email: finvers@test.ru</div>
-                        <div className="footer__block-link">Office: +7 900 000 00 00</div>
+                        <div className="footer__block-link footer__link-phone">Office: +7 900 000 00 00</div>
                     </div>
                 </div>
                 <div className="footer__block">
                     <div className="footer__block-name">Fast Links</div>
                     <div className="footer__block-links">
-                        <div className="footer__block-link">Home</div>
-                        <div className="footer__block-link">About Us</div>
-                        <div className="footer__block-link">Cryptocurrencies</div>
-                        <div className="footer__block-link">Crypto News</div>
-                        <div className="footer__block-link">My Account</div>
+                        <div className="footer__block-link"><a href="#">Home</a></div>
+                        <div className="footer__block-link"><a href="#">About Us</a></div>
+                        <div className="footer__block-link"><a href="#">Cryptocurrencies</a></div>
+                        <div className="footer__block-link"><a href="#">Crypto News</a></div>
+                        <div className="footer__block-link"><a href="#">My Account</a></div>
                     </div>
                 </div>
                 <div className="footer__block">
                     <div className="footer__block-name">Newsletter</div>
                     <div className="footer__block-links">
-                        <div className="w-40 h-auto text-justify text-sm font-medium text-white/70">Keep up with our latest news and events. Subscribe to our newsletter!</div>
-                        <div>
-                            <input type="email" className="w-50 rounded-3xl h-8 text-black p-2"/>
+                        <div className="footer__block-info">Keep up with our latest news and events. Subscribe to our newsletter!</div>
+                        <div className="footer__input-container">
+                            <input type="email"
+                                   placeholder="Your Email..."
+                                   className="footer__input"/>
+                            <button
+                                type="submit"
+                                className="footer__btn">
+                                <CarbonSendAlt className="footer__send-icon" />
+                            </button>
                         </div>
                     </div>
                 </div>
